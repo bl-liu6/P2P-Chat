d11e86b93ce778b13e048d6eff664ab534885148
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:10:38 -0700

    Update DataBase.class.php

--- web/DataBase.class.php
+++ web/DataBase.class.php
@@ -1,24 +1,22 @@
 <?php
 
 /****************************************************
- * Author Howard.
- * Date: 2017/11/9
- * Version 1.0
- * 支持SELECT/UPDATE/INSERT命令的预处理及
- * SELETE/UPDATE/INSERT/DELETE命令的直接执行
- * 在WHERE子句中 仅可使用“=”操作符 其他操作符请自行组装sql语句
+ * Bolun Liu
+ * 03/30/2023
+ * supports prepared statements for SELECT/UPDATE/INSERT commands
+ * SELETE/UPDATE/INSERT/DELETE commands
  ****************************************************/
 class DataBase
 {
 
     public $pdo = null;
-    private $dbType = "mysql";    //数据库类型
-    private $dbPort = "3306";     //数据库端口号
-    private $dbHost;              //数据库主机
-    private $dbName;              //数据库名
-    private $userName;            //用户名
-    private $password;            //用户密码
-    public $debug = false;        //调试模式
+    private $dbType = "mysql";    //db Type
+    private $dbPort = "3306";     //db Port
+    private $dbHost;              //db Host
+    private $dbName;              //db Name
+    private $userName;            //userName
+    private $password;            //password
+    public $debug = false;        //debug
     private $prepare;
 
     public function __construct($dbHost, $dbName, $userName, $password)
@@ -34,11 +32,11 @@ class DataBase
     {
         try {
             $dbr = $this->dbType . ':host=' . $this->dbHost . ';port=' . $this->dbPort . ';dbname=' . $this->dbName;
-            $this->pdo = new PDO($dbr, $this->userName, $this->password, array(PDO::ATTR_PERSISTENT => true));          //设置为长连接
+            $this->pdo = new PDO($dbr, $this->userName, $this->password, array(PDO::ATTR_PERSISTENT => true));      
             $this->pdo->exec('SET NAMES utf8');
-            $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);                                 //关闭本地预处理
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);                            //错误模式,此项为警告模式
-//            $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);                                       //错误模式,此项为异常模式
+            $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);                             
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);                 
+//            $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);                              
         } catch (PDOException $e) {
             die('Connection failed: ' . $e->getMessage());
         }
@@ -46,16 +44,15 @@ class DataBase
 
 
     /* *
-     * 普通的SELECT语句
-     * 使用PDO::query方法
-     * @param  string   $table      数据表名称
-     * @param  array    $fields     选择的列名称 空值时为返回所有
-     * @param  array    $where      where条件 仅支持“=”作为判断操作符 键名为字段名 键值为对应值
-     * @param  string   $orderby    指定列排序
-     * @param  boolean  $sort       降序/升序排列  默认为升序 false为降序
-     * @param  string   $limit      限制返回结果数量及起始位置 单参数时为返回值数量 双参数时为起始位置和数量
-     * @param  string   $fetch      返回数组关联方式 默认为FETCH_BOTH assoc为FETCH_ASSOC num时为FETCH_NUM both时为FETCH_BOTH
-     * @return array                返回查询结果数组
+     * PDO::query methods
+     * @param  string   $table     
+     * @param  array    $fields     
+     * @param  array    $where     
+     * @param  string   $orderby    
+     * @param  boolean  $sort      
+     * @param  string   $limit    
+     * @param  string   $fetch    
+     * @return array           
      * */
     public function select($table, $fields = "", $where = "", $orderby = "", $sort = true, $limit = "", $fetch = "both")
     {
@@ -69,16 +66,14 @@ class DataBase
 
 
     /* *
-     * 使用预处理的SELECT语句
-     * 内部使用PDO的prepare预处理 并自动绑定参数 防止注入
-     * @param  string   $table      数据表名称
-     * @param  array    $fields     选择的列名称 空值时为返回所有
-     * @param  array    $where      where条件 仅支持“=”作为判断操作符 键名为字段名 键值为对应值
-     * @param  string   $orderby    指定列排序
-     * @param  boolean  $sort       降序/升序排列  默认为升序 false为降序
-     * @param  string   $limit      限制返回结果数量及起始位置 单参数时为返回值数量 双参数时为起始位置和数量
-     * @param  string   $fetch      返回数组关联方式 默认为FETCH_BOTH assoc为FETCH_ASSOC num时为FETCH_NUM both时为FETCH_BOTH
-     * @return array                返回查询结果数组
+     * @param  string   $table      
+     * @param  array    $fields     
+     * @param  array    $where      
+     * @param  string   $orderby    
+     * @param  boolean  $sort       
+     * @param  string   $limit      
+     * @param  string   $fetch      
+     * @return array                
      * */
     public function select_($table, $fields = "", $where = "", $orderby = "", $sort = true, $limit = "", $fetch = "both")
     {
@@ -92,11 +87,11 @@ class DataBase
 
 
     /* *
-     * 普通的INSERT语句
-     * 使用PDOStatement::exec方法
-     * @param string $table 数据表名
-     * @param array  $data  键名为列名 键值为插入值
-     * @return int          返回受影响记录数
+     * common INSERT
+     * PDOStatement::exec methods
+     * @param string $table 
+     * @param array  $data  
+     * @return int         
      * */
     public function insert($table, $data)
     {
@@ -111,11 +106,10 @@ class DataBase
 
 
     /* *
-     * 使用预处理的INSERT语句
-     * 内部使用PDO的prepare预处理 并自动绑定参数 防止注入
-     * @param string $table     数据表名
-     * @param array  $data      键名为列名 键值为插入值
-     * @return int              返回受影响记录数
+     * prepared INSERT
+     * @param string $table    
+     * @param array  $data      
+     * @return int            
      * */
     public function insert_($table, $data)
     {
@@ -129,12 +123,12 @@ class DataBase
 
 
     /*
-     * 普通的UPDATE语句
-     * 使用PDO::exec方法
-     * @param  string   $table      数据表名称
-     * @param  array    $data       键名为列名 键值为要更改值
-     * @param  array    $where      where条件 仅支持“=”作为判断操作符 键名为字段名 键值为对应值
-     * @return int                  返回影响数目
+     * common UPDATE
+     * PDO::exec methods
+     * @param  string   $table      
+     * @param  array    $data      
+     * @param  array    $where     
+     * @return int                  
      * */
     public function update($table, $data, $where = "")
     {
@@ -148,16 +142,15 @@ class DataBase
 
 
     /* *
-     * 使用预处理的UPDATE语句
-     * 内部使用PDO的prepare预处理 并自动绑定参数 防止注入
-     * @param  string   $table      数据表名称
-     * @param  array    $fields     选择的列名称 空值时为返回所有
-     * @param  array    $where      where条件 仅支持“=”作为判断操作符 键名为字段名 键值为对应值
-     * @param  string   $orderby    指定列排序
-     * @param  boolean  $sort       降序/升序排列  默认为升序 false为降序
-     * @param  string   $limit      限制返回结果数量及起始位置 单参数时为返回值数量 双参数时为起始位置和数量
-     * @param  string   $fetch      返回数组关联方式 默认为FETCH_BOTH assoc为FETCH_ASSOC num时为FETCH_NUM both时为FETCH_BOTH
-     * @return array                返回查询结果数组
+     * prepared UPDATE
+     * @param  string   $table      
+     * @param  array    $fields    
+     * @param  array    $where    
+     * @param  string   $orderby   
+     * @param  boolean  $sort      
+     * @param  string   $limit    
+     * @param  string   $fetch      
+     * @return array                
      * */
     public function update_($table, $data, $where = "")
     {
@@ -171,12 +164,12 @@ class DataBase
 
 
     /*
-     * 普通的DELETE语句
-     * 使用PDO::exec方法
-     * @param  string   $table      数据表名称
-     * @param  array    $data       键名为列名 键值为要更改值
-     * @param  array    $where      where条件 仅支持“=”作为判断操作符 键名为字段名 键值为对应值
-     * @return int                  返回影响数目
+     * common DELETE
+     * PDO::exec methods
+     * @param  string   $table     
+     * @param  array    $data      
+     * @param  array    $where     
+     * @return int                 
      * */
     public function delete($table, $where = "")
     {
@@ -190,11 +183,10 @@ class DataBase
 
 
     /* *
-     * sql语句构造方法
-     * SELECT时默认升序
-     * @param  string  $command   命令标志符 用于判断执行的命令类型
-     * @param  array   $paramArr  命令参数
-     * @return sting              返回组装好的sql语句
+     * SQL statement construction method
+     * @param  string  $command   
+     * @param  array   $paramArr  
+     * @return sting            
      *
      * */
     private function sqlCreat($command, $paramArr)
@@ -248,10 +240,10 @@ class DataBase
 
 
     /* *
-     * 构造WHERE子句的方法
-     * @param  string $conlon    标记是否需要绑定参数 需要绑定参数时 构造的where子句中为带有冒号的变量形式 否则为无冒号的对应的值
-     * @param  array  $array     WHERE子句的列名和变量名一般分别对应本数组的键名和键值
-     * @return string            构造好的WHERE子句
+     * Constructing the WHERE clause
+     * @param  string $conlon    
+     * @param  array  $array   
+     * @return string           
      * */
     private function whereCreat($conlon, $array)
     {
@@ -271,11 +263,9 @@ class DataBase
 
 
     /* *
-     * 由于UPDATE命令的语句格式较为特殊 有两处需要构造因此此命令的构造单独使用一个方法
-     * @param  string $command  标记是何处以及何种方式的构造  dataEqual为无需绑定参数时构造UPDATE的列名和新值
-     *                          dataQuest为需要绑定参数时的列名和新值 whereQuest为WHERE子句的构造 需绑定参数
-     * @param  array  $arr      构造时的数组 列名和变量名一般分别对应本数组的键名和键值
-     * @return string           构造好的子句
+     * @param  string $command  
+     * @param  array  $arr      
+     * @return string          
      * */
     private function updateSqlCreat($command, $arr)
     {
@@ -302,11 +292,10 @@ class DataBase
 
 
     /* *
-     * SELECT命令预处理绑定参数并执行的方法
-     * @param  string  $sql     要执行的已经构造好的sql语句
-     * @param  array   $where   用于绑定参数 键名键值一般对应WHERE子句中变量名和值
-     * @param  string  $fetch   标记返回结果数组的形式 assoc为关联数组 num为索引数组 both为二者都有
-     * @return array            数组形式的返回结果
+     * @param  string  $sql   
+     * @param  array   $where 
+     * @param  string  $fetch  
+     * @return array          
      * */
     private function select_Exec($sql, $where, $fetch)
     {
@@ -335,10 +324,10 @@ class DataBase
 
 
     /*
-     * SELECT命令直接执行的方法
-     * @param  string  $sql     要执行的已经构造好的sql语句
-     * @param  string  $fetch   标记返回结果数组的形式 assoc为关联数组 num为索引数组 both为二者都有
-     * @return array            数组形式的返回结果
+     * SELECT command
+     * @param  string  $sql    
+     * @param  string  $fetch  
+     * @return array          
      * */
     public function selectExec($sql, $fetch)
     {
@@ -361,10 +350,10 @@ class DataBase
 
 
     /* *
-     * INSERT命令的预处理绑定参数并执行
-     * @param   string $sql   要执行的已经构造好的sql语句
-     * @param   array  $data  用于绑定参数的数组 键名键值一般对应变量名和值
-     * @return  int           返回受影响记录的数目
+     * INSERT command
+     * @param   string $sql   
+     * @param   array  $data  
+     * @return  int          
      * */
     private function insert_Exec($sql, $data)
     {
@@ -381,11 +370,11 @@ class DataBase
 
 
     /* *
-     * UPDATE命令的预处理绑定参数并执行
-     * @param   string $sql   要执行的已经构造好的sql语句
-     * @param   array  $data  用于绑定参数的数组 键名键值一般对应变量名和值
-     * @param   array  $where 用于绑定参数 键名键值一般对应WHERE子句中变量名和值
-     * @return  int           返回受影响记录的数目
+     * UPDATE command
+     * @param   string $sql   
+     * @param   array  $data  
+     * @param   array  $where 
+     * @return  int           
      * */
     private function update_Exec($sql, $data, $where)
     {
@@ -402,9 +391,9 @@ class DataBase
 
 
     /* *
-     * INSERT/UPDATE/DELETE 等命令直接执行的方法
-     * @param   string $sql   构造好的要执行的sql语句
-     * @return  int           返回受影响记录的数目
+     * INSERT/UPDATE/DELETE 
+     * @param   string $sql   
+     * @return  int          
      * */
     private function normalExec($sql)
     {
@@ -418,9 +407,8 @@ class DataBase
 
 
     /* *
-     * sql语句经过预处理语句后绑定参数的方法
-     * @flag    string  $flag   用于标记绑定参数的类型  quest为变量为问号的类型 colon为变量为冒号加变量名的格式
-     * @param   array   $param  用于绑定的数组 键名键值一般分别对应为变量名和值
+     * @flag    string  $flag   
+     * @param   array   $param  
      * @return  null
      * */
     private function bindParam($flag, $param)
