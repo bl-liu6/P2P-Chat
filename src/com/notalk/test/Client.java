b60b5ecdf334b0fff5380dd728d2796d3e1e0fa2
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:44:19 -0700

    Update Client.java

--- src/com/notalk/test/Client.java
+++ src/com/notalk/test/Client.java
@@ -10,29 +10,25 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /*
- * 客户端
+ * client
  */
 public class Client {
     public static void main(String[] args) {
         try {
-            //1.创建客户端Socket，指定服务器地址和端口
             Socket socket=new Socket("localhost", 8888);
-            //2.获取输出流，向服务器端发送信息
-            OutputStream os=socket.getOutputStream();//字节输出流
-            PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
+            OutputStream os=socket.getOutputStream();
+            PrintWriter pw=new PrintWriter(os);
             Scanner scanner = new Scanner(System.in);
             String str = scanner.next();
             pw.write(str);
             pw.flush();
-            socket.shutdownOutput();//关闭输出流
-            //3.获取输入流，并读取服务器端的响应信息
+            socket.shutdownOutput();
             InputStream is=socket.getInputStream();
             BufferedReader br=new BufferedReader(new InputStreamReader(is));
             String info=null;
             while((info=br.readLine())!=null){
-                System.out.println("我是客户端，服务器说："+info);
+                System.out.println("I am client"+info);
             }
-            //4.关闭资源
             br.close();
             is.close();
             pw.close();
