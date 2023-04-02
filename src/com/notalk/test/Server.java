9ac558c83f39ec1e9a07036cfef45b5eaa7deaa3
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:46:01 -0700

    Update Server.java

--- src/com/notalk/test/Server.java
+++ src/com/notalk/test/Server.java
@@ -9,32 +9,23 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/*
- * 基于TCP协议的Socket通信，实现用户登陆
- * 服务器端
- */
+
 public class Server {
     public static void main(String[] args) {
         try {
-            //1.创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
             ServerSocket serverSocket=new ServerSocket(8888);
             Socket socket=null;
-            //记录客户端的数量
             int count=0;
-            System.out.println("***服务器即将启动，等待客户端的连接***");
-            //循环监听等待客户端的连接
+            System.out.println("***waiting for client connection***");
             while(true){
-                //调用accept()方法开始监听，等待客户端的连接
                 socket=serverSocket.accept();
-                //创建一个新的线程
                 ServerThread serverThread=new ServerThread(socket);
-                //启动线程
                 serverThread.start();
 
-                count++;//统计客户端的数量
-                System.out.println("客户端的数量："+count);
+                count++;
+                System.out.println("client number："+count);
                 InetAddress address=socket.getInetAddress();
-                System.out.println("当前客户端的IP："+address.getHostAddress());
+                System.out.println("client IP："+address.getHostAddress());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,18 +35,13 @@ public class Server {
 
 
 
-/*
- * 服务器线程处理类
- */
 class ServerThread extends Thread {
-    // 和本线程相关的Socket
     Socket socket = null;
 
     public ServerThread(Socket socket) {
         this.socket = socket;
     }
 
-    //线程执行的操作，响应客户端的请求
     public void run(){
         InputStream is=null;
         InputStreamReader isr=null;
@@ -63,25 +49,22 @@ class ServerThread extends Thread {
         OutputStream os=null;
         PrintWriter pw=null;
         try {
-            //获取输入流，并读取客户端信息
             is = socket.getInputStream();
             isr = new InputStreamReader(is);
             br = new BufferedReader(isr);
             String info=null;
-            while((info=br.readLine())!=null){//循环读取客户端的信息
-                System.out.println("我是服务器，客户端说："+info);
+            while((info=br.readLine())!=null){
+                System.out.println("i am client"+info);
             }
-            socket.shutdownInput();//关闭输入流
-            //获取输出流，响应客户端的请求
+            socket.shutdownInput();
             os = socket.getOutputStream();
             pw = new PrintWriter(os);
-            pw.write("欢迎您！");
-            pw.flush();//调用flush()方法将缓冲输出
+            pw.write("Welcome!");
+            pw.flush();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }finally{
-            //关闭资源
             try {
                 if(pw!=null)
                     pw.close();
