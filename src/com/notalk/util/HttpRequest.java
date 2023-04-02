14e67eae35bcd8a6751983da96cfa73f3f884eb6
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:52:01 -0700

    Update HttpRequest.java

--- src/com/notalk/util/HttpRequest.java
+++ src/com/notalk/util/HttpRequest.java
@@ -11,13 +11,13 @@ import java.util.Map;
 
 public class HttpRequest {
     /**
-     * 向指定URL发送GET方法的请求
+     * Send a GET request to a specified URL
      *
      * @param url
-     *            发送请求的URL
+     *            Send URL
      * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return URL 所代表远程资源的响应结果
+     *            Request parameters, which should be in the form of name1=value1&name2=value2。
+     * @return URL The response result representing the remote resource
      */
     public static String sendGet(String url, String param) {
         String result = "";
@@ -25,22 +25,22 @@ public class HttpRequest {
         try {
             String urlNameString = url + "?" + param;
             URL realUrl = new URL(urlNameString);
-            // 打开和URL之间的连接
+            // Open a connection between the URL and the client
             URLConnection connection = realUrl.openConnection();
-            // 设置通用的请求属性
+            // Set common request properties
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
             connection.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 建立实际的连接
+            // Establish the actual connection
             connection.connect();
-            // 获取所有响应头字段
+            // Get all response header fields
             Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
+            // Iterate through all response header fields
 //            for (String key : map.keySet()) {
 //                System.out.println(key + "--->" + map.get(key));
 //            }
-            // 定义 BufferedReader输入流来读取URL的响应
+            // Define a BufferedReader input stream to read the response from the URL
             in = new BufferedReader(new InputStreamReader(
                     connection.getInputStream()));
             String line;
@@ -48,10 +48,9 @@ public class HttpRequest {
                 result += line;
             }
         } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
+            System.out.println("An exception occurred while sending the GET request!" + e);
             e.printStackTrace();
         }
-        // 使用finally块来关闭输入流
         finally {
             try {
                 if (in != null) {
@@ -65,13 +64,13 @@ public class HttpRequest {
     }
 
     /**
-     * 向指定 URL 发送POST方法的请求
+     * Send a POST request to a specified URL
      *
      * @param url
-     *            发送请求的 URL
+     *            The URL to send the request to
      * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return 所代表远程资源的响应结果
+     *            The request parameters, which should be in the form of name1=value1&name2=value2
+     * @return The response result that represents the remote resource
      */
     public static String sendPost(String url, String param) {
         PrintWriter out = null;
@@ -79,23 +78,21 @@ public class HttpRequest {
         String result = "";
         try {
             URL realUrl = new URL(url);
-            // 打开和URL之间的连接
+            // Open a connection between the URL and the client
             URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
+            // Setting the general request properties
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 发送POST请求必须设置如下两行
+            // Send POST request
             conn.setDoOutput(true);
             conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
+            // Get the output stream of the URLConnection object
             out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
+            // Send request param
             out.print(param);
-            // flush输出流的缓冲
             out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
             in = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()));
             String line;
@@ -103,10 +100,9 @@ public class HttpRequest {
                 result += line;
             }
         } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
+            System.out.println("An exception occurred while sending the POST request!"+e);
             e.printStackTrace();
         }
-        //使用finally块来关闭输出流、输入流
         finally{
             try{
                 if(out!=null){
