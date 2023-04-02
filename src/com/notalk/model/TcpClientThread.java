7e3bcc8317c277105ee41421e8c236449ae4456c
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:38:00 -0700

    Update TcpClientThread.java

--- src/com/notalk/model/TcpClientThread.java
+++ src/com/notalk/model/TcpClientThread.java
@@ -38,14 +38,11 @@ public class TcpClientThread extends Thread{
             Scanner scanner = new Scanner(System.in);
 //            setName(scanner);
 
-            // 接收服务器端发送过来的信息的线程启动
             ExecutorService exec = Executors.newCachedThreadPool();
             exec.execute(new ListenrServser());
 
-            // 建立输出流，给服务端发信息
             this.pw = new PrintWriter(
                     new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"), true);
-            //首次连接 发送账号
             pw.println(MainApp.Mysid);
 
         } catch(Exception e) {
@@ -66,32 +63,29 @@ public class TcpClientThread extends Thread{
 
     private void setName(Scanner scan) throws Exception {
         String name;
-        //创建输出流
         PrintWriter pw = new PrintWriter(
                 new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"),true);
-        //创建输入流
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream(),"UTF-8"));
 
         while(true) {
-            System.out.println("请创建您的昵称：");
+            System.out.println("create your nickname：");
             name = scan.nextLine();
             if (name.trim().equals("")) {
-                System.out.println("昵称不得为空");
+                System.out.println("nickname cannot be empty");
             } else {
                 pw.println(name);
                 String pass = br.readLine();
                 if (pass != null && (!pass.equals("OK"))) {
-                    System.out.println("昵称已经被占用，请重新输入：");
+                    System.out.println("nickname already used, try a new one：");
                 } else {
-                    System.out.println("昵称“"+name+"”已设置成功，可以开始聊天了");
+                    System.out.println("nickname“"+name+"”set successfully!");
                     break;
                 }
             }
         }
     }
 
-    // 循环读取服务端发送过来的信息并输出到客户端的控制台
     class ListenrServser implements Runnable {
         Gson gson = new Gson();
         @Override
@@ -111,7 +105,7 @@ public class TcpClientThread extends Thread{
     }
 
     /**
-    * 发送消息
+    * send message
     * */
     public int sendMsg(String msg){
             this.pw.println(msg);
