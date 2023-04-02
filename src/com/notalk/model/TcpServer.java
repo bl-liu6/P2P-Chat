bfa4bc8b25ea39d9912f1713b04db31a5ed612c9
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:43:13 -0700

    Update TcpServer.java

--- src/com/notalk/model/TcpServer.java
+++ src/com/notalk/model/TcpServer.java
@@ -1,11 +1,3 @@
-/**
-*
-*  客户端第一次连接时发送自己的账号作为唯一认证
-*  并且与连接成功的socket作为键值对存入HashMap中
-*
-* */
-
-
 package com.notalk.model;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -27,14 +19,10 @@ public class TcpServer {
     private ServerSocket serverSocket;
     private Gson gson = new Gson();
 
-     /**
-     * 创建线程池来管理客户端的连接线程
-     * 避免系统资源过度浪费
-     **/
     private ExecutorService exec;
 
      /**
-     * 客户端信息
+     * client info
      **/
     private Map<String,PrintWriter> storeInfo;
 
@@ -50,7 +38,7 @@ public class TcpServer {
     }
 
     /**
-    * 将客户端的信息以Map形式存入集合中
+    * store client information in a collection as a Map
     * */
     private void putIn(String key,PrintWriter value) {
         synchronized(this) {
@@ -58,22 +46,19 @@ public class TcpServer {
         }
     }
 
-     /**
-     * 将给定的输出流从共享集合中删除
-     **/
     private synchronized void remove(String  key) {
         storeInfo.remove(key);
     }
 
     /**
-    * 将给定的消息转发给所有客户端
+    * forward the given message to all clients
     **/
     private synchronized void sendToAll(String mySid,String message,String type) {
-        //TODO 获取好友列表
+        //TODO get friends list
         try {
             String sidListJson = db.getFriendsSidList(Integer.parseInt(mySid));
             List<String> friendSidList = gson.fromJson(sidListJson,List.class);
-            //发送上线通知至各好友
+            //send online notification to friends
             for(String sid: friendSidList) {
                 if(storeInfo.containsKey(sid)){
                     storeInfo.get(sid).println();
@@ -87,21 +72,15 @@ public class TcpServer {
 
     }
 
-    /**
-     *将给定的消息转发给私聊的客户端(加个同步锁~)
-     *若好友在线则直接发送并存储到数据库中
-     *若不在线则同时存储到未读数据库中
-     *待上线时检查是否有未读消息
-     *
-     */
+
     private synchronized void sendToSomeone(String mySid,String toSid,String content,String time,String msg) throws SQLException {
         if(storeInfo.containsKey(toSid)){
             PrintWriter pw = storeInfo.get(toSid);
             if(pw != null) pw.println(msg);
-            System.out.println("发送……");
+            System.out.println("sending……");
         }else{
             db.sendfriendUnreadMsg(Integer.parseInt(mySid),Integer.parseInt(toSid),content,time);
-            System.out.println("存至未读数据库……");
+            System.out.println("saving to unread database……");
         }
         db.sendfriendMsg(Integer.parseInt(mySid),Integer.parseInt(toSid),content,time);
     }
@@ -110,27 +89,19 @@ public class TcpServer {
     public void start() {
         try {
             while(true) {
-                System.out.println("服务端已启动... ... 等待客户端连接... ... ");
+                System.out.println("waiting for client to connect... ... ");
                 Socket socket = serverSocket.accept();
 
-                // 获取客户端的ip地址
+                // get client ip address
                 InetAddress address = socket.getInetAddress();
-                System.out.println("客户端：“" + address.getHostAddress() + "”连接成功！ ");
-                /*
-                * 启动一个线程，由线程来处理客户端的请求，这样可以再次监听
-                * 下一个客户端的连接
-                */
-                exec.execute(new ListenrClient(socket)); //通过线程池来分配线程
+                System.out.println("client:“" + address.getHostAddress() + "”successfully connected!");
+                exec.execute(new ListenrClient(socket)); 
             }
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * 该线程体用来处理给定的某一个客户端的消息，循环接收客户端发送
-     * 的每一个字符串，并输出到控制台
-     */
     class ListenrClient implements Runnable {
 
         private Socket socket;
@@ -140,18 +111,13 @@ public class TcpServer {
             this.socket = socket;
         }
 
-        /**
-        * 首次链接即登录时通过内部类来获取账号！
-        * */
         private String getSid() throws Exception {
             try {
-                //服务端的输入流读取客户端发送来的输出流
                 BufferedReader bReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-                //服务端将昵称验证结果通过自身的输出流发送给客户端
                 PrintWriter ipw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"),true);
 
                 //TODO
-                //读取客户端发来的昵称s
+                //read nickname
                 while(true) {
                     String sid = bReader.readLine();
                     if ((sid.trim().length() == 0) || storeInfo.containsKey(sid)) {
@@ -170,20 +136,17 @@ public class TcpServer {
         public void run() {
             try {
 
-                //通过客户端的Socket获取客户端的输出流
-                //用来将消息发送给客户端
                 PrintWriter pw = new PrintWriter(
                         new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
 
-                 //将账号和其所说的内容存入共享集合HashMap中
+                 // store account information and their messages in a shared HashMap collection
 
                 sid = getSid();
                 putIn(sid, pw);
 //                Thread.sleep(100);
 
 
-                // 通过客户端的Socket获取输入流
-                //读取客户端发送来的信息
+                //read client message
                 BufferedReader bReader = new BufferedReader(
                         new InputStreamReader(socket.getInputStream(), "UTF-8"));
                 String msgString = null;
@@ -197,12 +160,12 @@ public class TcpServer {
                     }else if(msg.getType().equals("p2g")){
                     }else if(msg.getType().equals("onLine")){
                         sendToAll(msg.getMysid(),msgString,"onLine");
-                        //数据库改变状态
+                        //database state change
                         db.setOnline(Integer.parseInt(msg.getMysid()));
                     }else if(msg.getType().equals("offLine")) {
                         sendToAll(msg.getMysid(), msgString, "offLine");
-                        System.out.println(msg.getMysid()+"下线了……");
-                        //数据库改变状态
+                        System.out.println(msg.getMysid()+"offline……");
+                        //database state change
                         db.setOffline(Integer.parseInt(msg.getMysid()));
                         remove(sid);
                         socket.close();
@@ -220,8 +183,8 @@ public class TcpServer {
                 // e.printStackTrace();
             } finally {
 //                remove(name);
-                // 通知所有客户端，某某客户已经下线
-//                sendToAll("[系统通知] "+name + "已经下线了。");
+                // notify all clients, xx is offline
+//                sendToAll("[System] "+name + "is offline.");
 
                 if(socket!=null) {
                     try {
