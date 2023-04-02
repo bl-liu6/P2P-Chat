349d117acd30320f939e68233526a56f6dc6108f
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2023-04-02 07:46:22 -0700

    Update test.java

--- src/com/notalk/test/test.java
+++ src/com/notalk/test/test.java
@@ -17,12 +17,6 @@ import java.util.concurrent.TimeUnit;
 
 public class test {
 //    static ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
-    /**
-     *每隔一段时间打印系统时间，互不影响的<br/>
-     * 创建并执行一个在给定初始延迟后首次启用的定期操作，后续操作具有给定的周期；<br/>
-     * 也就是将在 initialDelay 后开始执行，然后在initialDelay+period 后执行，<br/>
-     * 接着在 initialDelay + 2 * period 后执行，依此类推。
-     */
 
     public static void main(String[] args) throws SQLException {
         Gson gson = new Gson();
